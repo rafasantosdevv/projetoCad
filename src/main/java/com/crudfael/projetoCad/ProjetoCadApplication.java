11f1c414e9adf0145e8b3b786c905abb759da807
Rafael <rafaeldosantos__@outlook.com>
2026-05-24 23:11:16 -0300

    criação

---
+++ src/main/java/com/crudfael/projetoCad/ProjetoCadApplication.java
@@ -0,0 +1,13 @@
+package com.crudfael.projetoCad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudfael/projetoCad/ProjetoCadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudfael.projetoCad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
